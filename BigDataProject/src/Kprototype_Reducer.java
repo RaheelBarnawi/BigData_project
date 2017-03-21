faa39cd2182f80398bbfd39f1b97a7b295f0551b
RaheelBarnawi <raheel_barnawi@hotmail.com>
2017-03-20 23:56:22 -0400

    added comments

--- BigDataProject/src/Kprototype_Reducer.java
+++ BigDataProject/src/Kprototype_Reducer.java
@@ -4,20 +4,21 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
-
-
+//import org.apache.hadoop.mapreduce.Reducer.Context;
 
 public class Kprototype_Reducer extends Reducer<IntWritable, Text, IntWritable, Text>  {
 
-	// 
-	//this method is used to find the most frequent value for  each  categorical dimension 
+	 	public final static Log logR = LogFactory.getLog(Kprototype_Reducer.class);
+
+	//this method is used to find the most frequent value (mode) for  each  categorical dimension 
 	public static String find_most_frequent(Map<String, Integer> dim)
 	{
-		
 		int freq=0; 
 		int max_freq=-1; 
 		String mode= null; 
@@ -35,88 +36,92 @@ public class Kprototype_Reducer extends Reducer<IntWritable, Text, IntWritable, 
 
 	}
 	
+	 @Override 
 	public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
+		 /*
+		  * Emit a cluster id as a key and  cluster representatives as a value
+		  * a cluster in this algorithm has tow representatives: a centriod for numeric features
+		  * and a clusteroid for categorical features.
+		  * centriod is the average  and
+		  *  clusteriod is the most frequent value in each  dimension
+		 */
 		
-		NumberFormat f = NumberFormat.getInstance(); // Gets a NumberFormat with the default locale, you can specify a Locale as first parameter (like Locale.FRENCH)
-		Map<Integer, Dimension_freq > dim_info= new HashMap<Integer, Dimension_freq >(); 
-		ArrayList<String> cate_values= new ArrayList<String>();
-		ArrayList<Double> sum_num_values= new ArrayList<Double>(5); 
+		// ------- variables for handling numeric values-----------------
+		NumberFormat f = NumberFormat.getInstance(); 
+		int numeric_counter= ClusterSummuray.num_feature; 
 		ArrayList<Double> center= new ArrayList<Double>(); 
-		Object[] list = new Object[5];
-		int dim_cate=6; // categorical dimension
-		int dim_num=5; 
-		Dimension_freq  object; 
-		Dimension_freq  temp_object; 
-		int temp_index=0; 
+		ArrayList<Double> sum_num_values= new ArrayList<Double>(numeric_counter); 
+		Object[] list_dim_sum = new Object[numeric_counter]; 
 		int nPoints = 0;
 		double temp_value=0.0;
 		double current_value=0.0;
+		double tempvalue=0.0; 
+		double avg=0.0; 
 		
+		// ------- variables for handling categorical values-----------------
+		Map<Integer, Dimension_freq > dim_info= new HashMap<Integer, Dimension_freq >(); 
+		int categorical_counter= ClusterSummuray.cate_feature; 
+		Dimension_freq  object; 
+		Dimension_freq  temp_object; 
+		int temp_index=0; 
+		ArrayList<String> clusteriod =new ArrayList<String>(); 
+		String mode_dim_i= null; 
 		
-		//	int temp_value=0; 
-		// 1- handling numerical part
-		// compute the mean of the values
-		// Initialize  sum_num_values 
-		for(int initi=0;initi<dim_num; initi++ )
+
+		// Initialize  sum of each numeric dimension  to zero 
+		for(int initi=0;initi<numeric_counter; initi++ )
 			sum_num_values.add(initi, 0.0);
+		
 		// prepare categorical dimensions
-		//for each dimension create an object to hold the frequency
-		//of values in that dimension 
-		for (int i=0; i<dim_cate; i++)
+		//for each dimension; create an object to hold the frequency of values in that dimension 
+		for (int i=0; i<categorical_counter; i++)
 		{
-			//logR.info("create dim_cate ");
 			object= new Dimension_freq (i);
 			dim_info.put(i, object);
 		}
+		
 		while (values.iterator().hasNext()) 
 		{
 			nPoints++;
 			String[] mixed_values = values.iterator().next().toString().split(" ");
 			// sum numeric values in dimension i 
-			for(int i=0; i<dim_num; i++)
+			for(int i=0; i<numeric_counter; i++)
 			{
-
-				//logR.info("sum_num" );
-
-				// current_value= Double.parseDouble(mixed_values[i]);
 				try {
 					current_value= f.parse(mixed_values[i]).doubleValue();
 				} catch (ParseException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} 
 				temp_value= sum_num_values.get(i) + current_value;
-				//sum_num_values.add(i, temp_value);  
-				list[i]= temp_value;
+				list_dim_sum[i]= temp_value;
 
 			}
 			
 
-			for(int j=0; j<dim_cate; j++)
+			// record the frequency of each value in j dimensions
+			for(int j=0; j<categorical_counter; j++)
 			{
-				temp_index=dim_num+j; 
-				//logR.info("temp_index " + temp_index);
+				temp_index=numeric_counter+j; 
 				temp_object= dim_info.get(j);
 				temp_object.put(mixed_values[temp_index],1);
 				dim_info.put(j, temp_object);
 			}
 
-		}// end reading list of values 
+		}
 
-		double tempvalue=0.0; 
-		// compute the mean for each i_num dimension 
-		double avg=0.0; 
-		for(int i=0; i<list.length; i++)
+		
+		// compute the mean for each numeric dimension 
+		
+		for(int i=0; i<list_dim_sum.length; i++)
 		{
-			tempvalue= (double) list[i];
+			tempvalue= (double) list_dim_sum[i];
 			avg=tempvalue / nPoints; 
 			center.add(i, avg);
 
 		}
-		// compute the mode for each i_cate dimension	
-		ArrayList<String> clusteriod =new ArrayList<String>(); 
-		String mode_dim_i= null; 
+		// find the mode for each  categorical dimension	
+		
 		for(int i=0; i<dim_info.size();i++)
 		{
 			temp_object=  dim_info.get(i) ; 
@@ -125,21 +130,21 @@ public class Kprototype_Reducer extends Reducer<IntWritable, Text, IntWritable, 
 			clusteriod.add(i, mode_dim_i);
 
 		}
-		String cluster_representive = "";
+		String cluster_representives = "";
 		for(int i=0; i<center.size(); i++)
 		{
-			cluster_representive+= center.get(i).toString();
-			if(i!=4)
-				cluster_representive+=" ";	
+			cluster_representives+= center.get(i).toString();
+			if(i!=numeric_counter-1) 
+				cluster_representives+=" ";	
 		}
 		for(int j=0; j<clusteriod.size();j++)
 		{
-			cluster_representive+=" ";
-			cluster_representive+= clusteriod.get(j);
+			cluster_representives+=" ";
+			cluster_representives+= clusteriod.get(j);
 
 		}
 
-		context.write(key, new Text(cluster_representive));
+		context.write(key, new Text(cluster_representives));
 
 	}
 }
